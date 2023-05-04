eab01c805fc49d2f6ceced864c5d798cf7742fff
Hotae Kim <hotae.kim.94@gmail.com>
2023-05-04 17:09:57 -0400

    complete day 2 & checkpoint 1

---
+++ Checkpoints/Checkpoint_1.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+let tempCelcius: Double = 25.0
+let tempFahrenheit: Double = tempCelcius * 9 / 5 + 32
+
+print("The temperature outside today is \(tempCelcius) in Celcius or \(tempFahrenheit) in Fahrenheit")
